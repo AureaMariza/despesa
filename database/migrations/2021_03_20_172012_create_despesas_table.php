56a6ce44eec3c4d49a73116d0fda3de1c5e7cfcb
Aurea Mariza <aureamariza@gmail.com>
2021-03-20 16:07:48 -0300

    framework e login

---
+++ database/migrations/2021_03_20_172012_create_despesas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDespesasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('despesas', function (Blueprint $table) {
+            $table->id();
+            $table->string('descricao', 300);
+            $table->date('data');
+            $table->string('anexo', 100);
+            $table->integer('user_id');
+            $table->decimal('valor', 10, 2);
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('despesas');
+    }
+}
